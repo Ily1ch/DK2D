274f5204a3a3b84cf98e443ff2cb97a407712439
Ily1ch <kudelkoilya@gmail.com>
2023-06-07 15:27:44 +0300

    6

--- Assets/scripts/RangeEnemy.cs
+++ Assets/scripts/RangeEnemy.cs
@@ -4,136 +4,81 @@ using UnityEngine;
 
 public class RangeEnemy : MonoBehaviour
 {
+    [Header("Attack Parameters")]
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
     [SerializeField] private int damage;
+
+    [Header("Ranged Attack")]
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private GameObject[] fireballs;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
-    [SerializeField] private Transform projectileSpawnPoint;
-    [SerializeField] private GameObject projectilePrefab;
     private float cooldownTimer = Mathf.Infinity;
-    private heromove currentPlayerHp;
+
+    //References
     private Animator anim;
-
-    public float currentHealth;
-    public float timeToDie;
-    public float detectionRadius;
-    public float speed;
-    public float distanceStopToAttack;
-    public Transform player;
-    private SpriteRenderer spriteRenderer;
-    private bool isFacingRight = true;
-
-    private void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
 
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            collision.GetComponent<heromove>().TakeDamage(damage);
+    }
     private void Update()
     {
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-
-        if (IsPlayerDetected(distanceToPlayer))
-            FlipSpriteTowardsPlayer();
-
-        if (IsPlayerDetected(distanceToPlayer) && distanceToPlayer > distanceStopToAttack)
-            FollowPlayer(distanceToPlayer);
-        else
-            anim.SetBool("walk", false);
-
         cooldownTimer += Time.deltaTime;
 
+        //Attack only when player in sight?
         if (PlayerInSight())
         {
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
-                anim.SetBool("walk", false);
-                anim.SetTrigger("attacking");
-
-                // Spawn projectile
-                Vector2 direction = player.position - transform.position;
-                GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, Quaternion.identity);
-                Projectile projectileScript = projectile.GetComponent<Projectile>();
-                
+                anim.SetTrigger("rangedAttack");
             }
         }
+
+    }
+
+    private void RangedAttack()
+    {
+        cooldownTimer = 0;
+        fireballs[FindFireball()].transform.position = firepoint.position;
+        fireballs[FindFireball()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
     }
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, player.position - transform.position, range, playerLayer);
-
-        if (hit.collider != null)
-        {
-            currentPlayerHp = hit.transform.GetComponent<heromove>();
-        }
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
 
         return hit.collider != null;
     }
-
-    private void DamagePlayer()
+    private void OnDrawGizmos()
     {
-        if (PlayerInSight())
-        {
-            currentPlayerHp.TakeDamage(damage);
-        }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        anim.SetTrigger("takedamage");
-
-        if (currentHealth <= 0)
-            Die();
-        else
-        {
-            StartCoroutine(TemporarySpeedReduction());
-        }
-    }
-
-    private IEnumerator TemporarySpeedReduction()
-    {
-        speed = 0f;
-        yield return new WaitForSeconds(2f);
-        speed = 3f;
-    }
-
-    private void Die()
-    {
-        speed = 0;
-        anim.SetTrigger("die");
-        Destroy(gameObject, timeToDie);
-    }
-
-    private bool IsPlayerDetected(float distanceToPlayer)
-    {
-        return distanceToPlayer <= detectionRadius;
-    }
-
-    private void FollowPlayer(float distanceToPlayer)
-    {
-        anim.SetBool("walk", true);
-        Vector2 direction = player.position - transform.position;
-        transform.Translate(direction.normalized * speed * Time.deltaTime);
-    }
-
-    private void FlipSpriteTowardsPlayer()
-    {
-        Vector2 direction = player.position - transform.position;
-        bool shouldFaceRight = (direction.x > 0);
-
-        if (shouldFaceRight != isFacingRight)
-        {
-            Vector3 newScale = transform.localScale;
-            newScale.x *= -1;
-            transform.localScale = newScale;
-            isFacingRight = shouldFaceRight;
-        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 }
