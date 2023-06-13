cfd5a69b2fa625cd3dd8b688a4393ebd9e836d5f
Ily1ch <kudelkoilya@gmail.com>
2023-06-13 22:20:11 +0300

    ;

--- Assets/ElevatingObject.cs
+++ Assets/ElevatingObject.cs
@@ -4,42 +4,38 @@ public class ElevatingObject : MonoBehaviour
 {
     [SerializeField] float height; // Высота подъема объекта
     [SerializeField] float speed; // Скорость подъема объекта
-    [SerializeField] Collider2D other;
-    Vector3 targetPosition;
+
     private bool isTriggered = false; // Флаг, указывающий, активирован ли триггер
+    private bool isPlayerOnLift = false; // Флаг, указывающий, находится ли игрок на лифте
+    private Vector3 targetPosition; // Целевая позиция подъема объекта
+
     private void Start()
     {
         targetPosition = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
     }
-    void Update()
+
+    private void Update()
     {
-        if (isTriggered)
+        if (isTriggered && !isPlayerOnLift)
         {
-           
-            
             transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
-            
-            // Ограничение значения Y позиции объекта
-           // transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, transform.position.y, targetHeight), transform.position.z);
         }
-        
-       
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            isPlayerOnLift = true;
             isTriggered = true; // Установка флага активации триггера
-        }
-        else
-        {
-            isTriggered = false; // Установка флага активации триггера
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider2D other)
     {
-       
+        if (other.CompareTag("Player"))
+        {
+            isPlayerOnLift = false;
+        }
     }
 }
