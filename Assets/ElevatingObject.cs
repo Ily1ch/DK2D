169fed54a7729b209e720e33ac5afba447c12ac1
Ily1ch <kudelkoilya@gmail.com>
2023-06-14 22:29:00 +0300

    .

--- Assets/ElevatingObject.cs
+++ Assets/ElevatingObject.cs
@@ -5,12 +5,34 @@ public class ElevatingObject : MonoBehaviour
     [SerializeField] private float targetX; // Целевая координата X для перемещения игрока
     [SerializeField] private float targetY; // Целевая координата Y для перемещения игрока
     [SerializeField] private float targetZ; // Целевая координата Z для перемещения игрока
-
+    public Animator anim;
     private void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.CompareTag("Player"))
         {
-            other.transform.position = new Vector3(targetX, targetY, targetZ);
+            anim.SetBool("StartOpen", true);
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetBool("StartOpen", false);
         }
     }
+
+
+    public void Teleportation(Collider2D other)
+    {
+        other.transform.position = new Vector3(targetX, targetY, targetZ);
+    }
 }
+
+
+//if (other.CompareTag("Player"))
+//{
+//    other.transform.position = new Vector3(targetX, targetY, targetZ);
+//}
